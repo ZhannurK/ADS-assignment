f0f241293af25fe5aaf53a5dc7f9e8302a146716
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-03 12:49:22 +0500

    zhannur

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(average(n, arr));
+    }
+
+    public static double average(int n, int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return (double) sum / arr.length;
+    }
+
+}
