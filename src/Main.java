bcfac44b03d0a68830844600f5906be00316b7cb
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-05 15:54:42 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
-import java.util.Scanner;
-
-public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++){
-            arr[i] = scanner.nextInt();
+public class Main {
+    public static void main(String[] args) {
+        int n = 7;
+        if (isPrime(n)) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Composite");
         }
-        System.out.println(average(n, arr));
     }
 
-    public static double average(int n, int[] arr){
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
         }
-        return (double) sum / arr.length;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
