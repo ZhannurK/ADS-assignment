7e68d5ff4c1f413df92c32a1247a81b849d250d6
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-06 10:24:48 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
         int n = scanner.nextInt();
-        System.out.println(fibonacci(n));
+        System.out.println(power(a, n));
     }
 
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
+    public static long power(int a, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return a * power(a, n - 1);
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
