037ba692a50699095d439b164097d23f0aeb8699
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-06 10:13:18 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
     public static void main(String[] args) {
-        int n = 7;
-        if (isPrime(n)) {
-            System.out.println("Prime");
-        } else {
-            System.out.println("Composite");
-        }
-    }
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        if (n < 0)
+            System.out.println("error");
+        else
+            System.out.println(factorial(n));
     }
-
 }
