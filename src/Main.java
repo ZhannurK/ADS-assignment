5db78cbf1bf273c83c948cb2f747b0688f1c9254
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-06 20:28:50 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    static int binomialCoefficient(int n, int m){
-        if (m > n){
-            return 0;
+    static int GCD(int a, int b){
+        if (b == 0){
+            return a;
         }
-        if (m == 0 || n == 0){
-            return 1;
+        else {
+            return GCD(b, a % b);
         }
-        return binomialCoefficient(n - 1, m - 1) + binomialCoefficient(n - 1, m);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        System.out.println(binomialCoefficient(n, m));
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(GCD(a, b));
     }
 }
