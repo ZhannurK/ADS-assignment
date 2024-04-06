04f53adb3245305e44df0c8c2e8d9e5c429f3650
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-06 18:50:52 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    private static boolean isAllDigits(int n, String s){
-        if (s.isEmpty()){
-            return true;
+    static int binomialCoefficient(int n, int m){
+        if (m > n){
+            return 0;
         }
-        if(!Character.isDigit(s.charAt(n))){
-            return false;
+        if (m == 0 || n == 0){
+            return 1;
         }
-        return isAllDigits(n+1,s);
+        return binomialCoefficient(n - 1, m - 1) + binomialCoefficient(n - 1, m);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String n = scanner.nextLine();
-        if (isAllDigits(0, n)) {
-            System.out.println("Yes");
-        }
-        else {
-            System.out.println("No");
-        }
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        System.out.println(binomialCoefficient(n, m));
     }
 }
