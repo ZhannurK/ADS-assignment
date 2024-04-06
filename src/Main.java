0dccaf8b982190245a0f92af63333556386329c8
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-06 15:41:45 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
-        if (n < 0)
-            System.out.println("error");
-        else
             System.out.println(factorial(n));
     }
 }
