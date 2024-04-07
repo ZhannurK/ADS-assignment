7e50f2833ed4b99b23acc5f13b0fdc68234c98ef
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-07 22:05:01 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
-
+/**
+ * Program to calculate the factorial of a number.
+ */
 public class Main {
+    /**
+     * Recursive method to calculate the factorial of a number.
+     *
+     * @param n The number for which factorial is calculated.
+     * @return The factorial of the given number.
+     */
     static int factorial(int n) {
         if (n == 0 || n == 1)
             return 1;
         else
             return n * factorial(n - 1);
     }
-
+    /**
+     * Entry point of the program.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
