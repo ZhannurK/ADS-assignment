23513849c47a04c445d9699c0c75d24d28b028aa
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-07 22:58:03 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
      * @param n The number for which factorial is calculated.
      * @return The factorial of the given number.
      */
-    static int factorial(int n) {
+    public static int factorial(int n) {
         if (n == 0 || n == 1)
             return 1;
         else
