81be2c5df289daaef359375967d0bbe0b681cffb
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-07 22:27:18 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
-
+/**
+ * Program to check if a string contains only digits.
+ */
 public class Main {
+    /**
+     * Recursive method to check if a string contains only digits.
+     *
+     * @param n The index of the character to check.
+     * @param s The string to check.
+     * @return True if all characters in the string are digits, false otherwise.
+     */
     private static boolean isAllDigits(int n, String s){
         if (s.isEmpty()){
             return true;
@@ -10,7 +19,11 @@ public class Main {
         }
         return isAllDigits(n+1,s);
     }
-
+    /**
+     * Entry point of the program.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String n = scanner.nextLine();
