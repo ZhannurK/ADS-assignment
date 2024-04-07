d9df1b0809ad0b0e5afe67b34b4e0fdda8f5f042
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-07 22:32:07 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
-
+/**
+ * Program  to find the greatest common divisor (GCD) of two numbers "a" and "b" using the Euclidean Algorithm.
+ */
 public class Main {
+    /**
+     * Method to calculate the greatest common divisor (GCD) of two numbers using Euclid's algorithm.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The greatest common divisor (GCD) of 'a' and 'b'.
+     */
     static int GCD(int a, int b){
         if (b == 0){
             return a;
@@ -9,7 +18,11 @@ public class Main {
             return GCD(b, a % b);
         }
     }
-
+    /**
+     * Entry point of the program.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
