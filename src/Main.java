8c96638dee8f415a22d08623b4be3100f0ffbab9
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-07 22:30:25 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
-
+/**
+ * Program to calculate the binomial coefficient C(n, k) using the formula C(n, k) = C(n-1, k-1) + C(n-1, k),
+ * where C(n, 0) = C(n, n) = 1
+ */
 public class Main {
+    /**
+     * Method to calculate the binomial coefficient (n choose m).
+     *
+     * @param n The total number of items.
+     * @param m The number of items to choose.
+     * @return The binomial coefficient.
+     */
     static int binomialCoefficient(int n, int m){
         if (m > n){
             return 0;
@@ -10,7 +20,11 @@ public class Main {
         }
         return binomialCoefficient(n - 1, m - 1) + binomialCoefficient(n - 1, m);
     }
-
+    /**
+     * Entry point of the program.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
