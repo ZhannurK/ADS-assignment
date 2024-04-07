4661e5924db92599de967ff3b43ed84fe8f30c13
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-07 22:12:18 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
-
+/**
+ * Program that takes two numbers, "a" and "n", and returns the result of raising "a" to the power of "n" (an).
+ */
 public class Main {
+    /**
+     * Entry point of the program.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int n = scanner.nextInt();
         System.out.println(power(a, n));
     }
-
+    /**
+     * Method to calculate the power of a number.
+     *
+     * @param a The base number.
+     * @param n The exponent.
+     * @return The result of raising 'a' to the power of 'n'.
+     */
     public static long power(int a, int n) {
         if (n == 0) {
             return 1;
