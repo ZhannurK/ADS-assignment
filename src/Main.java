e69860135a74cfce047c1b714258bd74730190cd
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-07 21:55:30 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
-
+/**
+ * Program that takes a number "n" and an array containing "n" elements,
+ * and returns the smallest element from the array.
+ */
 public class Main{
+    /**
+     * Entry point of the program.
+     *
+     * @param args Command line arguments.
+     */
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -10,7 +18,13 @@ public class Main{
         }
         System.out.println(FindMin(n, arr));
     }
-
+    /**
+     * Method to find the minimum element in an array.
+     *
+     * @param n   The size of the array.
+     * @param arr The array in which to find the minimum element.
+     * @return The minimum element in the array.
+     */
     public static double FindMin(int n, int[] arr){
         int minel = arr[0];
         for (int i = 0; i < n; i++){
@@ -20,5 +34,4 @@ public class Main{
         }
         return minel;
     }
-
 }
