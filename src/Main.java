688ff5f2a1dbb0f7c890ff6a9989f5f8b69660bc
Zhannur Kainedin <230030@astanait.edu.kz>
2024-04-13 11:30:39 +0500

    zhannur

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,20 @@
 import java.util.Scanner;
-/**
- * Program  to find the greatest common divisor (GCD) of two numbers "a" and "b" using the Euclidean Algorithm.
- */
+
 public class Main {
-    /**
-     * Method to calculate the greatest common divisor (GCD) of two numbers using Euclid's algorithm.
-     *
-     * @param a The first number.
-     * @param b The second number.
-     * @return The greatest common divisor (GCD) of 'a' and 'b'.
-     * Time complexity in this method is O(log(min(a, b))) because it uses the Euclidean algorithm for finding the greatest common divisor,
-     * which has logarithmic time complexity.
-     */
-    public static int GCD(int a, int b){
-        if (b == 0){
-            return a;
+    public static int countAccurance(int n, int m, String s) {
+        int count = 0;
+        if (m == s.length()){
+            return 0;
         }
-        else {
-            return GCD(b, a % b);
+        if (s.charAt(m) == n){
+            count++;
         }
+            count += countAccurance(n, m + 1, s);
+            return count;
     }
-    /**
-     * Entry point of the program.
-     *
-     * @param args Command line arguments.
-     */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(GCD(a, b));
+        String string = "abbabaab";
+        char a = 'a';
+        System.out.println(countAccurance(a, 0, string));
     }
 }
